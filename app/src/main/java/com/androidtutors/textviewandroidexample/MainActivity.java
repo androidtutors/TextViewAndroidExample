7b6d132b629b4557e5ac110d641e2ada54ab51b2
Android Tutors <atutors14@gmail.com>
2018-01-26 11:20:12 -0800

    Adding TODOs

--- app/src/main/java/com/androidtutors/textviewandroidexample/MainActivity.java
+++ app/src/main/java/com/androidtutors/textviewandroidexample/MainActivity.java
@@ -5,9 +5,21 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //TODO 6 : Add TextView variable with name helloTextView
+    //TODO 7 : Add TextView variable with name learnTextView
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO 8 Use findViewById to link helloTextView to TextView textview-hello
+        //TODO 9 Use findViewById to link learnTextView to TextView textview-learn
+
+        //TODO 10 set text of helloTextView to "Hello **your name**"
+        //TODO 11 set text of learnTextView to "Let's learn Android"
+
+
     }
 }
